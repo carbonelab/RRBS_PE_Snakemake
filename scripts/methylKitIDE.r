c53ebffe77d2a4a2e92df3af72152f1833415887
jackson-wells <jackson.taylor.wells@gmail.com>
2023-03-19 13:14:12 -0700

    added option parsing and skeleton code

--- scripts/methylKitIDE.r
+++ scripts/methylKitIDE.r
@@ -1,4 +1,6 @@
 library(methylKit)
+#!/usr/bin/env Rscript
+library("optparse")
 
 #command line inputs
 #
@@ -11,11 +13,34 @@ library(methylKit)
 # -c [1-100]        : minimum read coverage
 # -h [0,1]          : header line present on files?
 
+option_list = list(
+    make_option(c("-p", "--plots"), action="store_true", default="store_false", dest="plots", help="create plot output files"),
+    make_option(c("-t", "--tables"), action="store_true", default="store_false", dest="tables", help="create table output files"),
+    make_option(c("-a", "--all"), action="store_true", default="store_false", dest="all", help="create plot and table output files"),
+    make_option(c("-i", "--input"), type="character", default=NULL, help="input file directory", metavar="character"),
+    make_option(c("-o", "--output"), type="character", default=NULL, help="output file directory", metavar="character"),
+    make_option(c("-s", "--treatment"), type="character", default=NULL, help="treatement vector for input files", metavar="character"),
+    make_option(c("-c", "--coverage"), type="integer", default=NULL, help="minimum coverage amount", metavar="integer"),
+    make_option(c("-h", "--header"), action="store_true", default="store_true", dest="header", help="designate the whether the input files contain a header line")
+); 
+ 
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
 #check input directory exists
 #Enumerate list of input files
 
-#check if output directory exists
-#create if not
+#inputDirectory <- paste(getwd(), "/cov_files", sep="")
+#if(!file.exists(inputDirectory)) {
+if(!file.exists(opt$input)) {
+	print("input directory not found")
+	quit()
+}
+inputFiles <- list.files(opt$input, "*.cov.gz", full=T)
+if(!length(inputFiles) > 1) {
+	print("No input files found")
+	quit()
+}
 
 #check if input files are present
 #check if input files are accessible?
@@ -23,10 +48,35 @@ library(methylKit)
 
 #create list of sample names from file names
 
+inputNames <- gsub("_val.*", "", inputFiles)
+inputNames <- gsub(paste0(inputDirectory,"/"), "", inputNames)
+
+sampleFiles <- lapply(inputFiles, function(x) x)
+sampleNames <- lapply(inputNames, function(x) x)
+
+
+#check if output directory exists
+#create if not
+
+#outputDirectory <- paste(getwd(), "/output/", sep="")
+#if(!file.exists(outputDirectory)) {
+if(!file.exists(opt$output)) {
+	print("output directory not found, creating one now")
+	dir.create(opt$output)
+}
+
 #create data object
 #   include all input parameters as potential command line inputs
 
-#if (plots)
+myObj = methRead(sampleFiles,
+		   	sample.id = sampleNames,
+			header = opt$header,
+			assembly = "genome",
+			treatment = opt$treatment,
+			pipeline = "bismarkCoverage",
+			mincov = opt$coverage) 
+
+if (plots) {
     #Loop over samples
         #coverage plot
             #create
@@ -34,7 +84,9 @@ library(methylKit)
         #methylation plot
             #create
             #store
-#if (tables)
+}
+
+if (tables) {
     #Loop over samples
         #coverage table
             #create
@@ -42,14 +94,20 @@ library(methylKit)
         #methylation table
             #create
             #store
+}
 
 #merge samples
 
-#if (tables)
+meth = unite(myObj, destrand=FALSE,c.cores=12)
+
+if (tables) {
     #correlation table
     #cluster sample table
+}
 
-#if (Plots)
+
+if (plots) {
     #cluster sample ploit
     #PCA scree
     #PCA scatter plot
+}
