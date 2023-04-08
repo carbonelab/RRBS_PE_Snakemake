1958142647b1a61b7314bd92e27ea995465211d6
jackson-wells <jackson.taylor.wells@gmail.com>
2023-04-07 18:34:22 -0700

    initial groundwork

--- scripts/methylKitDMR.R
+++ scripts/methylKitDMR.R
@@ -3,3 +3,22 @@
 source("scripts/helperFunctions.R")
 
 #Validate script inputs
+args = commandArgs(trailingOnly=TRUE)
+samples <- checkSamples()
+config <- checkConfiguration()
+
+outputDirectory <- args[2]
+inputDirectory <- args[1]
+sampleFiles <- getSampleFiles(samples[,c(1)], inputDirectory)
+treatment <- getTreatmentVector(samples[,c(2)])
+sampleNames <- getList(samples[,c(1)])
+
+
+myObj <- getObject(treatment, sampleFiles, sampleNames, config)
+meth = getMergedRegions(myObj, config)
+
+#Merge/Filter
+
+#DMR 
+
+#Bed files
